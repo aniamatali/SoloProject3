b2a2648093c0cc32f2bdd8614eaafa368b0f0f49
Guest User <aniamatali@icloud.com>
2017-09-22 08:29:10 -0700

    backend logic done with returned output in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 function pingPong(userInput) {
-  
+  var output = [];
+  for (var i = 1; i <= userInput; i+= 1) {
+    if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else {
+      output.push(i);
+    }
   }
+  return output;
 }
+
 
 $(document).ready(function() {
   $("#formOne").submit(function(event){
     var userInput = parseInt($("#userInput").val());
     console.log(userInput);
-
+    var output = pingPong(userInput);
+    console.log(output);
     event.preventDefault();
 
   });
