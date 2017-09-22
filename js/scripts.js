e6d1997ad576c09c0ce13fdd41aa06be3205855f
Guest User <aniamatali@icloud.com>
2017-09-22 08:30:52 -0700

    changed the 3 and 15 on for loop so that ping-pong will show up....

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function pingPong(userInput) {
   var output = [];
   for (var i = 1; i <= userInput; i+= 1) {
-    if (i % 3 === 0) {
-      output.push("ping");
+    if (i % 15 === 0) {
+      output.push("ping-pong");
     } else if (i % 5 === 0) {
       output.push("pong");
-    } else if (i % 15 === 0) {
-      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
     } else {
       output.push(i);
     }
